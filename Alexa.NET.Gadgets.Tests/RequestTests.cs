a2105523bb7135f0d6b693c08cd9d62a73fccc20
Steven Pears <steven.pears@live.co.uk>
2017-12-26 14:49:50 +0000

    Update readme

--- Alexa.NET.Gadgets.Tests/RequestTests.cs
+++ Alexa.NET.Gadgets.Tests/RequestTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using Alexa.NET.Gadgets.GameEngine.Requests;
 using Alexa.NET.Request;
-using Alexa.NET.Request.Type;
 using Xunit;
 
 namespace Alexa.NET.Gadgets.Tests
