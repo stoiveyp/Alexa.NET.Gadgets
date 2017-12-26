3e4d8bc901d3094b86eddc8d0861bcbe2e65d7a5
Steven Pears <steven.pears@live.co.uk>
2017-12-26 08:50:34 +0000

    Rename GadgetEvent to InputHandlerEvent Initial request converter setup

---
+++ Alexa.NET.Gadgets.Tests/RequestTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.Gadgets.GameEngine.Requests;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Xunit;
+
+namespace Alexa.NET.Gadgets.Tests
+{
+    public class RequestTests
+    {
+        public RequestTests()
+        {
+            new GadgetRequestHandler().AddToRequestConverter();
+        }
+
+        [Fact]
+        public void InputHandlerEventRequestDeserializesCorrectly()
+        {
+            var actual = Utility.ExampleFileContent<SkillRequest>("InputHandlerEventRequest.json");
+        }
+    }
+}
---
+++ Alexa.NET.Gadgets/GameEngine/Requests/InputHandlerEventRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Alexa.NET.Gadgets.GameEngine.Requests
+{
+    public class InputHandlerEventRequest: Request.Type.Request
+    {
+        
+    }
+}
