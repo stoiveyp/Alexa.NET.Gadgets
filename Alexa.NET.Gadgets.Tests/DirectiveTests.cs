1c8bbeb38c14cb2b4b3a1aff55a4b7d2a763a495
Steven Pears <steven.pears@live.co.uk>
2017-12-27 00:28:47 +0000

    Initial setlight directive

--- Alexa.NET.Gadgets.Tests/DirectiveTests.cs
+++ Alexa.NET.Gadgets.Tests/DirectiveTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Alexa.NET.Gadgets.GadgetController;
 using Alexa.NET.Gadgets.GameEngine;
 using Alexa.NET.Gadgets.GameEngine.Directives;
 using Xunit;
@@ -8,6 +9,39 @@ namespace Alexa.NET.Gadgets.Tests
     public class DirectiveTests
     {
         [Fact]
+        public void SetLightDirectiveSerializesProperly()
+        {
+            var setLight = new SetLightDirective
+            {
+                TargetGadgets = new List<string> { "gadgetId1", "gadgetId2" },
+                Parameters = new List<SetLightParameter>
+                {
+                    new SetLightParameter
+                    {
+                        TriggerEvent = TriggerEvent.None,
+                        TriggerEventTimeMilliseconds = 0,
+                        Animations = new SetLightAnimation
+                            {
+                                Repeat=1,
+                                TargetLights = new List<int>{1},
+                                Sequence = new List<AnimationSegment>
+                                {
+                                    new AnimationSegment
+                                    {
+                                        Blend=false,
+                                        DurationMilliseconds = 10000,
+                                        Color="0000FF"
+                                    }
+                                }
+                            }
+                        }
+                }
+            };
+
+            Assert.True(Utility.CompareJson(setLight, "SetLightDirective.json"));
+        }
+
+        [Fact]
         public void StopInputHandlerDirectiveSerializesProperly()
         {
             var actual = new StopInputHandlerDirective
@@ -15,7 +49,7 @@ namespace Alexa.NET.Gadgets.Tests
                 OriginatingRequestId = "amzn1.echo-api.request.406fbc75-8bf8-4077-a73d-519f53d172a4"
             };
 
-            Assert.True(Utility.CompareJson(actual,"StopInputHandlerDirective.json"));
+            Assert.True(Utility.CompareJson(actual, "StopInputHandlerDirective.json"));
         }
 
         [Fact]
@@ -31,13 +65,13 @@ namespace Alexa.NET.Gadgets.Tests
                 TriggerTimeMilliseconds = 1000
             };
 
-            Assert.True(Utility.CompareJson(actual,"InputHandlerEvent.json"));
+            Assert.True(Utility.CompareJson(actual, "InputHandlerEvent.json"));
         }
 
         [Fact]
         public void StartInputHandlerDirectiveSerializesProperly()
         {
-            var actual = new StartInputHandlerDirective{MaximumHistoryLength = 512,TimeoutMilliseconds = 5000};
+            var actual = new StartInputHandlerDirective { MaximumHistoryLength = 512, TimeoutMilliseconds = 5000 };
             Assert.True(Utility.CompareJson(actual, "StartInputHandlerDirective.json"));
         }
     }
---
+++ Alexa.NET.Gadgets/GadgetController/AnimationSegment.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Alexa.NET.Gadgets.GadgetController
+{
+    public class AnimationSegment
+    {
+        [JsonProperty("durationMs")]
+        public int DurationMilliseconds { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("blend")]
+        public bool Blend { get; set; }
+    }
+}
