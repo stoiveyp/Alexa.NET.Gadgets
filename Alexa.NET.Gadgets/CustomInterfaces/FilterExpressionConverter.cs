b44d6f4df32fbfca417c552aeae9225a9e9515f1
Steven Pears <steven.pears@live.co.uk>
2019-08-19 23:13:43 +0100

    handle  expression deserialising converter

--- Alexa.NET.Gadgets/CustomInterfaces/FilterExpressionConverter.cs
+++ Alexa.NET.Gadgets/CustomInterfaces/FilterExpressionConverter.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 
 namespace Alexa.NET.Gadgets.CustomInterfaces
 {
@@ -24,34 +30,86 @@ namespace Alexa.NET.Gadgets.CustomInterfaces
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
+            while (reader.TokenType != JsonToken.StartObject)
+            {
+                reader.Read();
+            }
+
             reader.Read();
+
             var op = reader.Value.ToString();
-            reader.Read();
+           // reader.Read();
             switch (op)
             {
                 case "and":
                 case "or":
+                    reader.Read();
                     var combined = new CombinedFilterExpression { Operator = (CombinationOperator)Enum.Parse(typeof(CombinationOperator), char.ToUpper(op[0]) + op.Substring(1)) };
-                    return ProcessCombinedExpression(combined, reader, serializer);
+                    var combineitem = ProcessCombinedExpression(combined, reader, serializer);
+                    return combineitem;
                 default:
-                    var comparison = new ComparisonFilterExpression { Operator = (ComparisonOperator)Enum.Parse(typeof(ComparisonOperator), op) };
-                    return ProcessComparisonFilter(comparison, reader, serializer);
+                    var comparison = new ComparisonFilterExpression { Operator = GetComparison(op) };
+                    var compareitem = ProcessComparisonFilter(comparison, reader, serializer);
+                    return compareitem;
             }
         }
 
         private ComparisonFilterExpression ProcessComparisonFilter(ComparisonFilterExpression expression, JsonReader reader, JsonSerializer serializer)
         {
-            reader.Skip();
+            reader.Read(); //Start Array
+
+            reader.Read(); //Start Object
+            reader.Read(); // Property name
+            reader.Read(); // Property Value
+            expression.Variable = reader.Value.ToString();
+            reader.Read(); // End Object
+
+            reader.Read(); //Value
+            expression.Value = reader.Value;
+            while (reader.TokenType != JsonToken.EndObject)
+            {
+                reader.Read(); //End Array
+            }
+
             return expression;
         }
 
         private CombinedFilterExpression ProcessCombinedExpression(CombinedFilterExpression expression, JsonReader reader, JsonSerializer serializer)
         {
-            reader.Skip();
+            reader.Read(); // Start Array
+            var list = new List<FilterExpression>();
+            while (reader.TokenType != JsonToken.EndArray)
+            {
+                var filter = serializer.Deserialize<FilterExpression>(reader);
+                list.Add(filter);
+                if (reader.TokenType == JsonToken.EndObject)
+                {
+                    reader.Read();
+                }
+            }
+
+            expression.Filters = list.ToArray();
+            while (reader.TokenType != JsonToken.EndObject)
+            {
+                reader.Read(); //End Array
+            }
             return expression;
         }
 
         private static readonly Type ExpressionType = typeof(FilterExpression);
+        private static readonly FieldInfo[] ComparisonMembers = typeof(ComparisonOperator).GetFields(BindingFlags.Public | BindingFlags.Static);
+
+        private ComparisonOperator GetComparison(string enumValue)
+        {
+            foreach (var field in ComparisonMembers)
+            {
+                if(field.GetCustomAttributes<EnumMemberAttribute>().First().Value == enumValue)
+                {
+                    return (ComparisonOperator)field.GetRawConstantValue();
+                }
+            }
+            throw new InvalidOperationException("Unable to handle that value");
+        }
 
         public override bool CanConvert(Type objectType)
         {
